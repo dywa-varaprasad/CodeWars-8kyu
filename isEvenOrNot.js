b2ea96b0c50050c8d94ba36519b6b5436fbaf692
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-08-11 21:50:09 +0530

    check even or not
    this function will checks even or not

---
+++ isEvenOrNot.js
@@ -0,0 +1,41 @@
+/*into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+*/
+
+
+//my solution
+
+function testEven(n) {
+    //Your awesome code here!
+  if(n%2===0){
+    return true 
+  }
+  else if(n%2!==0){
+    return false function testEven(n) {
+    //Your awesome code here!
+  if(n%2===0){
+    return true 
+  }
+  else if(n%2!==0){
+    return false 
+  }
+  
+  else if (n<0){
+    return "Uneven"
+  }
+  
+}
+  }
+  
+  else if (n<0){
+    return "Uneven"
+  }
+  
+}

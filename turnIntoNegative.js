d0428c1172ea0cf7ec1c45cebb4ed3f742977b09
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-08-20 14:43:28 +0530

    Updated small fix

--- turnIntoNegative.js
+++ turnIntoNegative.js
@@ -5,7 +5,7 @@ Examples
 makeNegative(1);    // return -1
 makeNegative(-5);   // return -5
 makeNegative(0);    // return 0
-makeNegative(0.12); // return -0.12
+make negative(0.12); // return -0.12
 */
 
 

7e3df20aa7381c848463e7f7b0f119c833fb35ba
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-08-23 22:46:42 +0530

    Update trafficLightsSignals.js

--- trafficLightsSignals.js
+++ trafficLightsSignals.js
@@ -1,8 +1,9 @@
-//traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+/*traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
 
 Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
 For example, when the input is green, output should be yellow.
+*/
 
 // my solution
 

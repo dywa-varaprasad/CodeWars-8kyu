6c20950f94b95823842fda550acd115aa4b20e4f
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2023-03-12 12:04:28 +0530

    Create greetwithName.js

---
+++ greetwithName.js
@@ -0,0 +1,11 @@
+/*
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+*/
+
+function greet(name){
+  //your code here
+
+let greet =  `Hello, ${name} how are you doing today?`
+return greet 
+
+}
